15fc9d6d19e0cc248ededfaa272b33fb9fd269d2
Chad Hinds <chadrickj8@gmail.com>
2020-02-29 18:22:09 -0500

    Remove validator from mongoose file

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,50 +1,10 @@
 const mongoose = require('mongoose')
-const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
 })
 
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    age: {
-        type: Number,
-        age: 0,
-        validate(value) {
-            if(value < 0) {
-                throw new Error("Age must be a positive number")
-            }
-        }
-    },
-    // validator for email
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if(!validator.isEmail(value)) {
-               throw new Error('Email is not valid!') 
-            }
-        }
-    },
-    password: {
-        type:String,
-        required: true,
-        minLength: 7,
-        validate(value){
-            if (value.toLowerCase().includes('password')) {
-                throw new Error('password cannot contain phrase password!')
-            }
-        },
-        trim: true
-    }
-})
 
 // const me = new User({
 //     name: 'Devin',
