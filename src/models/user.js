26c4df920d7c5b7aa9888f11d1010bdc4749896c
Chad Hinds <chadrickj8@gmail.com>
2020-02-28 20:02:51 -0500

    Move user model to it's own dedicated file

---
+++ src/models/user.js
@@ -0,0 +1,39 @@
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    age: {
+        type: Number,
+        age: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error("Age must be a positive number")
+            }
+        }
+    },
+    // validator for email
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is not valid!')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 7,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('password cannot contain phrase password!')
+            }
+        },
+        trim: true
+    }
+})
