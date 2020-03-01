ea38b437763eb7866a3277d9f8477d03087f2259
Chad Hinds <chadrickj8@gmail.com>
2020-03-01 16:31:48 -0500

    Database check

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,40 +1,6 @@
 const mongoose = require('mongoose')
-
+// Connects to the database!
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
-})
-
-
-// const me = new User({
-//     name: 'Devin',
-//     email: '345@me.DEEZ.com'
-// })
-
-// me.save().then(() => {
-//     console.log(me)
-// }).catch((error) => {
-//     console.log('Error!', error)
-// })
-
-// Model for tasks!
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false,
-    }
-})
-const smack = new Task({
-    description: "Time for your favorite activity"
-})
-
-smack.save().then(() => {
-    console.log(smack)
-}).catch((error) => {
-    console.log('Error!', error)
 })
