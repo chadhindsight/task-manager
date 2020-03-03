c997be79a9a30b88eefab9a421ac4350b527976b
Chad Hinds <chadrickj8@gmail.com>
2020-03-03 15:02:39 -0500

    Fix typo task

--- src/index.js
+++ src/index.js
@@ -31,18 +31,20 @@ app.get('/users',(req,res) =>{
 app.get('/users/:id', (req, res) => {
     const _id = req.params.id
 
-    User.findById(_id).then(user=>{
-        if(!user) {
-            return status(404).send()
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send()
         }
-    }).catch(e=>{
+        res.send(user)
+    }).catch((e) => {
         res.status(500).send()
     })
 })
 // CREATE NEW TASK
 app.post('/tasks', (req,res) =>{
     const task = new Task(req.body)
-
+    res.send(task)
+    
     task.save().then(() => {
         res.status(201)
 
@@ -50,6 +52,14 @@ app.post('/tasks', (req,res) =>{
     }).catch(e => {
         res.status(400)
         res.send(e)
+    })
+})
+// FETCH ALL TASKS
+app.get('/tasks', (req, res) => {
+    User.find({}).then(tasks => {
+        res.send(tasks)
+    }).catch(e => {
+        res.status(500).send()
     })
 })
 
