7b73bbee0d76f0221dda5670f6814d47c537cc25
Chad Hinds <chadrickj8@gmail.com>
2020-03-06 17:42:45 -0500

    update task by id

--- src/index.js
+++ src/index.js
@@ -28,48 +28,69 @@ app.get('/users', async (req,res) =>{
     }
 })
 
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
     const _id = req.params.id
-
-    User.findById(_id).then((user) => {
-        if (!user) {
+    try {
+        const user = await User.findById(_id)
+        if(!user) {
             return res.status(404).send()
         }
         res.send(user)
-    }).catch((e) => {
+
+    } catch(e) {
         res.status(500).send()
-    })
+    }
 })
 // CREATE NEW TASK
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
 
-    task.save().then(() => {
+    try {
+        await task.save()
         res.status(201).send(task)
-    }).catch((e) => {
+    } catch (e) {
         res.status(400).send(e)
-    })
+    }
 })
+
 // FETCH ALL TASKS
-app.get('/tasks', (req, res) => {
-    Task.find({}).then((tasks) => {
+app.get('/tasks', async (req, res) => {
+    const tasks = await Task.find({})
+    try {
         res.send(tasks)
-    }).catch((e) => {
-        res.status(500).send()
-    })
+    } catch(e) {
+        res.status(400).send(e)
+    }
+    // Task.find({}).then((tasks) => {
+    //     res.send(tasks)
+    // }).catch((e) => {
+    //     res.status(500).send()
+    // })
 })
 
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
     const taskId = req.params.id
-
-    Task.findById(taskId).then((task) => {
+    try {
         if (!task) {
             return res.status(404).send()
         }
-        res.send(task)
-    }).catch((e) => {
+        const task = Task.findById(taskId)
+    }
+
+    catch(e) {
         res.status(500).send()
-    })
+    }
+})
+// update task by id!
+app.patch('/tasks/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update))
+
+    if(!isValidOperation) {
+        return res.status(400).send({error: 'Update invalid'})
+    }
 })
 
 app.listen(port, () =>{
