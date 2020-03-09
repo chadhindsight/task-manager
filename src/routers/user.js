f05a8bd29cf1be99acbf78f3c2f77b04b0f85e6d
Chad Hinds <chadrickj8@gmail.com>
2020-03-09 17:16:26 -0400

    Use bracket notation to get updated value

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const Useer = require('../models/user');
-const router = new express.Router();
+const express = require('express')
+const User = require('../models/user')
+const router = new express.Router()
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
@@ -15,7 +15,7 @@ router.post('/users', async (req, res) => {
 
 router.get('/users', async (req, res) => {
     try {
-        const users = await User.find()
+        const users = await User.find({})
         res.send(users)
     } catch (e) {
         res.status(500).send()
@@ -24,19 +24,49 @@ router.get('/users', async (req, res) => {
 
 router.get('/users/:id', async (req, res) => {
     const _id = req.params.id
+
     try {
         const user = await User.findById(_id)
+
         if (!user) {
             return res.status(404).send()
         }
-        res.send(user)
 
+        res.send(user)
     } catch (e) {
         res.status(500).send()
     }
 })
 
-// Delete
+router.patch('/users/:id', async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email', 'password', 'age']
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+
+    try {
+        const user = await User.findById(req.params.id)
+
+        updates.forEach(update=>{
+            // Bracket notation because values are dynamic
+            user[update] = req.body[update]
+
+            await user.save()
+        });
+
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
 router.delete('/users/:id', async (req, res) => {
     try {
         const user = await User.findByIdAndDelete(req.params.id)
@@ -44,10 +74,11 @@ router.delete('/users/:id', async (req, res) => {
         if (!user) {
             return res.status(404).send()
         }
+
         res.send(user)
-    }
-    catch (e) {
+    } catch (e) {
         res.status(500).send()
     }
 })
-module.exports = router;
+
+module.exports = router
