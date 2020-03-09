42537fd17f0517caf212ee443db4813186308f47
Chad Hinds <chadrickj8@gmail.com>
2020-03-09 14:59:13 -0400

    Use pre method to handle any user updates

--- src/models/user.js
+++ src/models/user.js
@@ -1,44 +1,55 @@
 const mongoose = require('mongoose')
 const validator = require('validator');
 
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    age: {
-        type: Number,
-        age: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error("Age must be a positive number")
-            }
-        }
-    },
-    // validator for email
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is not valid!')
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        minLength: 7,
-        validate(value) {
-            if (value.toLowerCase().includes('password')) {
-                throw new Error('password cannot contain the word password!')
+const userSchema = new mongoose.Schema({
+     
+        name: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        age: {
+            type: Number,
+            age: 0,
+            validate(value) {
+                if (value < 0) {
+                    throw new Error("Age must be a positive number")
+                }
             }
         },
-        trim: true
-    }
+        // validator for email
+        email: {
+            type: String,
+            required: true,
+            trim: true,
+            lowercase: true,
+            validate(value) {
+                if (!validator.isEmail(value)) {
+                    throw new Error('Email is not valid!')
+                }
+            }
+        },
+        password: {
+            type: String,
+            required: true,
+            minLength: 7,
+            validate(value) {
+                if (value.toLowerCase().includes('password')) {
+                    throw new Error('password cannot contain the word password!')
+                }
+            },
+            trim: true
+        }
+    })
+userSchema.pre('save', async function(next) {
+    const user = this;
+    
+    console.log('Just before saving')
+    
+    //next() signals that the middle operation is done and moves on to the next
+    next();
 })
+
+const User = mongoose.model('User', userSchema)
 
 module.exports = User;
