f376a3129beb572db82219531e732d22ee347b57
Chad Hinds <chadrickj8@gmail.com>
2020-03-10 00:11:41 -0400

    verify jwt token

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-const express = require('express');
-require('./db/mongoose');
-
-const userRouter = require('./routers/user');
-const taskRouter = require('./routers/task');
+const express = require('express')
+require('./db/mongoose')
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
+const jwt = require('jsonwebtoken');
 
 const app = express()
-const port = process.env.PORT || 3000;
-// Parse JSON for express
+const port = process.env.PORT || 3000
 
 app.use(express.json())
-app.use(userRouter);
+app.use(userRouter)
+app.use(taskRouter)
 
-
-app.listen(port, () =>{
-    console.log(`sesrver is up on ${port}`);
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
+// using bcrypt
+const bcrypt = require('bcrypt')
+
+const myFunction = async () =>{
+   const token = jwt.sign({_id: 'wwww'}, 'nicollegotit!');
+   console.log(token)
+
+    const data = jwt.verify(token, 'nicollegotit!')
+}
+myFunction()
