39ec09d2be85e7d5e546d703eda1247a47fddbf7
Chad Hinds <chadrickj8@gmail.com>
2020-03-12 14:39:58 -0400

    do not allow user to see password and token after they signin

--- src/models/user.js
+++ src/models/user.js
@@ -1,77 +1,98 @@
 const mongoose = require('mongoose')
-const validator = require('validator');
-const bcrypt = require('bcrypt');
+const validator = require('validator')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
-     
-        name: {
-            type: String,
-            required: true,
-            trim: true
-        },
-        age: {
-            type: Number,
-            age: 0,
-            validate(value) {
-                if (value < 0) {
-                    throw new Error("Age must be a positive number")
-                }
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
             }
-        },
-        // validator for email
-        email: {
-            type: String,
-            unique: true,
-            required: true,
-            trim: true,
-            lowercase: true,
-            validate(value) {
-                if (!validator.isEmail(value)) {
-                    throw new Error('Email is not valid!')
-                }
-            }
-        },
-        password: {
-            type: String,
-            required: true,
-            minLength: 7,
-            validate(value) {
-                if (value.toLowerCase().includes('password')) {
-                    throw new Error('password cannot contain the word password!')
-                }
-            },
-            trim: true
         }
-    })
-userSchema.methods.generateAuthToken = async function() {
-    
-}   
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain "password"')
+            }
+        }
+    },
+    age: {
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a postive number')
+            }
+        }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
+})
+userSchema.methods.getPublicProfile = function () {
+    const user = this;
+    const userObject = user.toObject()
+    // Doesnt allow user to see password and token after they signin
+    delete userObject.password
+    delete userObject.tokens    
+}
 
-userSchema.statics.findByCredentials = async(email, password)=> {
-    const user = await User.findOne({email})
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+    const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse')
 
-    if(!user){
-        throw new Error('Unable to log in')
+    user.tokens = user.tokens.concat({ token })
+    await user.save()
+
+    return token
+}
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        throw new Error('Unable to login')
     }
 
     const isMatch = await bcrypt.compare(password, user.password)
 
-    if(!isMatch) {
-        throw new Error('Cannot log in')
+    if (!isMatch) {
+        throw new Error('Unable to login')
     }
+
     return user
-}    
-// Hash the plain text password
-userSchema.pre('save', async function(next) {
-    const user = this;
-    
-    if(user.isModified('password')) {
-        user.password = await bcrypt.hash(user.password, 8);
+}
+
+// Hash the plain text password before saving
+userSchema.pre('save', async function (next) {
+    const user = this
+
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
     }
-    //next() signals that the middle operation is done and moves on to the next
-    next();
+
+    next()
 })
 
 const User = mongoose.model('User', userSchema)
 
-module.exports = User;
+module.exports = User
