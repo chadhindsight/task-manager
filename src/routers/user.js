53d9678d7f39ef2694ed9a40c334319d23f71bb2
Chad Hinds <chadrickj8@gmail.com>
2020-03-12 14:05:37 -0400

    use auth middleware to help logout process

--- src/routers/user.js
+++ src/routers/user.js
@@ -24,16 +24,26 @@ router.post('/users/login', async (req, res) => {
         res.status(400).send()
     }
 })
-router.post('/users/logout', auth, async(req,res) =>{
+router.post('/users/logout', auth, async(req, res) =>{
     try {
-        req.user.tokens =req.user.tokens.filter( token =>{
+        req.user.tokens = req.user.tokens.filter( token =>{
             return token.token !== req.token
         })
         await req.user.save()
-        
+
         res.send()
     } catch(e) {
         res.status(500).send
+    }
+})
+
+router.post('/users/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch (e) {
+        res.status(500).send()
     }
 })
 
