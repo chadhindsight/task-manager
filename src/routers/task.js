935bb979851b12b1b65f0fbc3701bf26f9a73380
Chad Hinds <chadrickj8@gmail.com>
2020-03-16 11:24:32 -0400

    change patch route callback

--- src/routers/task.js
+++ src/routers/task.js
@@ -19,8 +19,8 @@ router.post('/tasks', auth, async (req, res) => {
 
 router.get('/tasks', auth, async (req, res) => {
     try {
-        const task = await Task.find({owner: req.user._id})
-        res.send(tasks)
+        await req.user.populate('tasks').execPopulate()
+        res.send(req.user.tasks)
     } catch (e) {
         res.status(500).send()
     }
@@ -42,7 +42,7 @@ router.get('/tasks/:id', auth, async (req, res) => {
     }
 })
 
-router.patch('/tasks/:id', async (req, res) => {
+router.patch('/tasks/:id', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['description', 'completed']
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -52,7 +52,7 @@ router.patch('/tasks/:id', async (req, res) => {
     }
 
     try {
-        const task = await Task.findById(req.params.id)
+        const task = await Task.findOne({_id: req.params.id, owner: req.user._id})
 
         updates.forEach((update) => task[update] = req.body[update])
         await task.save()
