013233423b56dbe2ee95b78dc2d7d7d61d730da3
Chad Hinds <chadrickj8@gmail.com>
2020-03-16 11:29:52 -0400

    Update delete callback

--- src/routers/task.js
+++ src/routers/task.js
@@ -54,23 +54,22 @@ router.patch('/tasks/:id', auth, async (req, res) => {
     try {
         const task = await Task.findOne({_id: req.params.id, owner: req.user._id})
 
-        updates.forEach((update) => task[update] = req.body[update])
-        await task.save()
-
         if (!task) {
             return res.status(404).send()
         }
-
+        
+        updates.forEach((update) => task[update] = req.body[update])
+        await task.save()
+        
         res.send(task)
     } catch (e) {
         res.status(400).send(e)
     }
 })
 
-router.delete('/tasks/:id', async (req, res) => {
+router.delete('/tasks/:id', auth, async (req, res) => {
     try {
-        const task = await Task.findByIdAndDelete(req.params.id)
-
+        const task = await Task.findOneAndDelete({_id:req.params.id, owner: req.user._id})
         if (!task) {
             res.status(404).send()
         }
