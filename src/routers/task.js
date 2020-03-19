d0870d0fcfef8de731d29a082b40cbcd1728b668
Chad Hinds <chadrickj8@gmail.com>
2020-03-19 11:51:44 -0400

    Setup pagination on task results

--- src/routers/task.js
+++ src/routers/task.js
@@ -16,7 +16,7 @@ router.post('/tasks', auth, async (req, res) => {
         res.status(400).send(e)
     }
 })
-// GET /tasks?completed=true. Limit & Skip pagination
+// GET /tasks?completed=true. Limit & Skip pagination!
 
 router.get('/tasks', auth, async (req, res) => {
     const match = {}
