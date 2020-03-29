4dfc9b35964e500974df32191ada65bd22262584
Chad Hinds <chadrickj8@gmail.com>
2020-03-29 16:14:26 -0400

    Load task router first

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 require('./db/mongoose')
-const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
+const userRouter = require('./routers/user')
 
 const app = express()
 const port = process.env.PORT || 3000
