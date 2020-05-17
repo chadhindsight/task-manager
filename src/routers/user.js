57af9b3fe0f9e0a2101498168bf6a6b4907d165a
Chad Hinds <chadrickj8@gmail.com>
2020-05-17 15:10:30 -0400

    Change message for failed image upload

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,11 +1,12 @@
 const express = require('express')
+const multer = require('multer')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 const router = new express.Router()
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
-    
+
     try {
         await user.save()
         const token = await user.generateAuthToken()
@@ -24,16 +25,17 @@ router.post('/users/login', async (req, res) => {
         res.status(400).send()
     }
 })
-router.post('/users/logout', auth, async(req, res) =>{
+
+router.post('/users/logout', auth, async (req, res) => {
     try {
-        req.user.tokens = req.user.tokens.filter( token =>{
+        req.user.tokens = req.user.tokens.filter((token) => {
             return token.token !== req.token
         })
         await req.user.save()
 
         res.send()
-    } catch(e) {
-        res.status(500).send
+    } catch (e) {
+        res.status(500).send()
     }
 })
 
@@ -51,7 +53,6 @@ router.get('/users/me', auth, async (req, res) => {
     res.send(req.user)
 })
 
-// Update a user
 router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'email', 'password', 'age']
@@ -62,7 +63,6 @@ router.patch('/users/me', auth, async (req, res) => {
     }
 
     try {
-        req.user
         updates.forEach((update) => req.user[update] = req.body[update])
         await req.user.save()
         res.send(req.user)
@@ -73,12 +73,31 @@ router.patch('/users/me', auth, async (req, res) => {
 
 router.delete('/users/me', auth, async (req, res) => {
     try {
-    //  Remove from the database
         await req.user.remove()
         res.send(req.user)
     } catch (e) {
         res.status(500).send()
     }
+})
+
+const upload = multer({
+    dest: 'avatars',
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please upload an image!'))
+        }
+
+        cb(undefined, true)
+    }
+})
+
+router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+    res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
 })
 
 module.exports = router
