c7915520e796abe0876b31aba452620a6a852b95
Chad Hinds <chadrickj8@gmail.com>
2020-07-14 21:41:00 -0400

    Add test folder

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,5 +1,14 @@
 // Jest globals are automatically provide so no need to use require()
-test('Hello world!', ()=>{
+const { calculateTip } = require('../src/math')
 
+test('Should calculate total with tip', ()=> {
+    const total = calculateTip(10, .2)
+    // Assertion
+   expect(total).toBe(12)
 })
 
+test('Should calculate total with default tip', () => {
+    const total = calculateTip(10)
+    // Assertion
+    expect(total).toBe(12.5)
+})
