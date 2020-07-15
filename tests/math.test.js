d8c82d742f4896df911743a181f3fc1e27edb8c2
Chad Hinds <chadrickj8@gmail.com>
2020-07-14 22:56:30 -0400

    Add new tests for temp checks

--- tests/math.test.js
+++ tests/math.test.js
@@ -1,14 +1,25 @@
 // Jest globals are automatically provide so no need to use require()
-const { calculateTip } = require('../src/math')
+const { calculateTip, celsiusToFahrenheit, fahrenheitToCelsius } = require('../src/math')
 
 test('Should calculate total with tip', ()=> {
     const total = calculateTip(10, .2)
     // Assertion
    expect(total).toBe(12)
 })
-
+ 
 test('Should calculate total with default tip', () => {
     const total = calculateTip(10)
     // Assertion
     expect(total).toBe(12.5)
+})
+
+test('Should convert 32 F to 0 C', ()=>{
+    let temperature = fahrenheitToCelsius(32)
+    expect(temperature).toBe(0)
+})
+
+
+test('Should convert 0 C to 32 F', () => {
+    const temp = celsiusToFahrenheit(0)
+    expect(temp).toBe(32) 
 })
